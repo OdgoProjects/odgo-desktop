615c7809c08d94065430ac7f3ad65b3a6addbd78
Angelo Fonseca <angelo.fonseca@odgo.com.br>
2019-04-17 17:32:21 -0300

    páginas estáticas com logos

--- src/Components/Moradia/Header/Header.js
+++ src/Components/Moradia/Header/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Header.css'
-// import Search from '../Search/Search'
+import logo from '../../../Img/Odgo-logo.svg'
 
 class Header extends Component {
     render(){
@@ -12,13 +12,13 @@ class Header extends Component {
                     <div className="main-nav">
                         <div>
                             <Link to="/" className="navbar-item odgo-logo">
-                                <img src="" alt="odgo logo" width="" height="" />
+                            <img src={ logo } alt="odgo logo" width="70" height="70" />
                             </Link>
                         </div>
                         <form action="" method="POST" className='form-answer'>
                                 <div className="input-box-answer">
                                     <div className="button-area">
-                                        <input type="search" />
+                                        <input type="search" placeholder="Procurando algo?" />
                                         <button>
                                             <i className="fa fa-search"></i>
                                         </button>
