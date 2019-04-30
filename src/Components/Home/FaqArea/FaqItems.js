28a713db890ce8461e74c3e9b92e2dd9e5a9cd2c
Angelo Fonseca <angelo.fonseca@odgo.com.br>
2019-04-30 16:24:56 -0300

    ajustando dimensões - home feita e falta yes/no da documentação

--- src/Components/Home/FaqArea/FaqItems.js
+++ src/Components/Home/FaqArea/FaqItems.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 import './FAQItems.css'
 import documentacaoIcon from '../../../Img/documentacao-icon.svg'
+import moradiaIcon from '../../../Img/moradia-icon.svg'
+import saudeIcon from '../../../Img/saude-icon.svg'
+import oportunidadesIcon from '../../../Img/oportunidades-icon.svg'
+import primeirospassosIcon from '../../../Img/primeirospassos-icon.svg'
 
 class FaqItems extends Component {
     render(){
@@ -14,19 +18,19 @@ class FaqItems extends Component {
                     <div className="box-1">
                         <div className="faq-items">
                             <div className="inside-items">
-                                <img src={ documentacaoIcon } alt="link para documentação" width="10" height="10" />
+                                <img src={ documentacaoIcon } alt="link para documentação" width="30" height="30" />
                                 <p>Documentação</p>
                             </div>
                         </div>
                         <div className="faq-items">
                             <div className="inside-items">
-                                <img src="" alt=""/>
+                                <img src={ moradiaIcon } alt="link para moradia" width="30" height="30" />
                                 <p>Moradia</p>  
                             </div>
                         </div>
                         <div className="faq-items">
                             <div className="inside-items">
-                                <img src="" alt=""/>
+                                <img src={ saudeIcon } alt="link para saúde" width="30" height="30" />
                                 <p>Saúde</p>
                             </div>
                         </div>
@@ -34,13 +38,13 @@ class FaqItems extends Component {
                     <div className="box-2">
                         <div className="faq-items">
                             <div className="inside-items">
-                                <img src="" alt=""/>
+                                <img src={ oportunidadesIcon } alt="link para oportunidades" width="30" height="30" />
                                 <p>Oportunidades</p>        
                             </div>
                         </div>
                         <div className="faq-items">
                             <div className="inside-items">
-                                <img src="" alt=""/>
+                                <img src={ primeirospassosIcon } alt="link para primeiros passos" width="30" height="30" />
                                 <p>Primeiros Passos</p>  
                             </div>
                         </div>
